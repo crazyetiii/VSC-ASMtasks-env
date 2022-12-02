fee807a25df0230d395a8aa66ccf13257d2dc06d
Chen Zhicheng <60308594+flytassel@users.noreply.github.com>
2022-12-02 16:58:57 +0800

    可以输出的版本

--- test2.asm
+++ test2.asm
@@ -95,13 +95,15 @@ print1:
     ADD AX, AX
     MOV DX,Score[BX];存入上一次的成绩
     MOV  BX, AX
+	MOV  ax, DX
+	CALL  output
     MOV  AX, Number[BX];输出编号
     CALL  output
     mov dl,32
     int 21h;第一个空格
     MOV  AX, Score[BX];输出成绩，存入这一次的成绩
     CALL  output
-    cmp dx,AX;上一次成绩和这一次成绩比较
+    cmp dx,AX
 	je do_nothing;这里每次都没有跳转，都执行了下面
     mov dl,32
     int 21h;第二个空格
